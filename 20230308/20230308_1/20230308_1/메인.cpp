9aeb34d1333cfd603a5eff98c22f5d2a56f84098
songuijun <125720533+songuijun@users.noreply.github.com>
2023-03-08 09:55:56 +0900

    Add files via upload

---
+++ 20230308/20230308_1/20230308_1/메인.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>//hearder 파일추가 standard
+
+
+int main(void)
+{
+
+	printf ("test \n"); //\n line feed
+	printf("202315010\n"); //\n line feed
+	printf("Song Ui Jun\n"); //\n line feed  
+	return 0;
+}
+
+//ctrl+f5 실행
