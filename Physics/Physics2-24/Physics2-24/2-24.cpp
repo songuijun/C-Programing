9e524dfc73bc4f772a3a2d5feeeb5c41430f04a6
songuijun <125720533+songuijun@users.noreply.github.com>
2023-05-16 21:31:45 +0900

    Update 2-24.cpp

--- Physics/Physics2-24/Physics2-24/2-24.cpp
+++ Physics/Physics2-24/Physics2-24/2-24.cpp
@@ -26,59 +26,58 @@ void Excersize_2_24(int solution, int answer)
 	printf("2-24 \n");
 	printf("높이가 %5.2lf m인 10층 옥상에서 \n", height);
 	printf("공을 수평방향으로 초속도 %5.2lf m/s로 던졌다. \n\n", second_velocity);
-	printf("(a) 공이 지면에 떨어지는 데 걸리는 시간은 얼마인가? \n");
+	printf("(a) 공이 지면에 떨어지는 데 걸리는 시간은 얼마인가? \n\n");
+	printf("(b) 공은 건물에서 수평으로 얼마의 거리에 떨어지는가? \n");
 
 	if (solution == SHOW)
 	{
 		printf("\n\n\n");
-		printf("==============================  풀이  ===================================\n");
+		printf("==============================  풀이  ===================================\n\n");
 		// your code here
-		t = sqrt(height / (1.0 / 2.0 * gravitational_acceleration) ) ;
-	
+		t = sqrt(height / (1.0 / 2.0 * gravitational_acceleration));
 
-		printf("등가속도 운동공식을 이용합니다. \n");
-		printf("x = x0 + v0 x t + (1/2) x a x t^2 \n");
-		printf("문자에 숫자를 대입합니다. \n");
-		printf("%5.2lf m = %5.2lf m + %5.2lf sec + 1/2 x %5.2lf m/s^2 x t^2 \n", height ,x0,v0, gravitational_acceleration);
-		printf("t^2 = %5.2lf m - %5.2lf m - %5.2lf sec / ( 1/2 x %5.2lf m/s^2 )\n",height,x0,v0,gravitational_acceleration);
-		printf("t = %5.2lf sec \n",t);
+		printf("(a) 공이 지면에 떨어지는 데 걸리는 시간은 얼마인가?\n\n");
 
-		printf("=================================================================\n");
-		printf("\n\n\n");
-	}
-	if (answer == SHOW){
-		printf("=========================   정 답   =============================\n");
+		printf("관련 공식은 x = x0 + v0 * t + (1/2) * a * t^2 \n");
+		printf("v: 나중 속도, v0: 초기 속도, a: 가속도, t: 시간. \n\n");
 
-		printf("t를 계산합니다. \n");
+		printf("연직 아래방향이므로 중력가속도(g)와 처음속도(v0)는 음수값을 가지게 됩니다.\n\n");
+
+		printf("%5.2lf m = (%5.2lf m) + (%5.2lf m) + t + 1/2 * %5.2lf m/s^2 * t^2 \n", height, x0, v0, gravitational_acceleration);
+		printf("t^2 = %5.2lf m - %5.2lf m - %5.2lf sec / ( 1/2 x %5.2lf m/s^2 )\n", height, x0, v0, gravitational_acceleration);
 		printf("t = %5.2lf sec \n", t);
 
-		printf("=================================================================\n");
-		printf("\n\n\n");
+		printf("\n");
+	}
+	if (answer == SHOW) {
+		printf("=========================   정 답   =============================\n\n");
+
+		printf("걸린 시간 t = %5.2lf sec \n", t);
+
+		printf("\n");
 	}
 
-	printf("(b) 공은 건물에서 수평으로 얼마의 거리에 떨어지는가?\n");
+
 
 	if (solution == SHOW)
 	{
-		printf("\n\n\n");
-		printf("==============================  풀이  ===================================\n");
+		printf("==============================  풀이  ===================================\n\n");
 		// your code here
 		x = second_velocity * t;
+		printf("(b) 공은 건물에서 수평으로 얼마의 거리에 떨어지는가? \n");
 
-		printf("등속 운동에서 물체의 이동 거리를 구하는 공식을 이용합니다. \n");
-		printf("x = x0 + v0 m/s x t sec \n");
-		printf("문자에 숫자를 대입합니다. \n");
-		printf("x = %5.2lf + %5.2lf m/s x %5.2lf sec \n", x0, second_velocity , t);
-		printf("x = %5.2lf m \n", x );
+		printf("관련 공식은 x = x0 + v0 m/s x t sec \n");
+		printf("x: 수평거리, x0: 초기거리, v0: 초기 속도, t: 시간. \n\n");
 
+		printf("x = %5.2lf + %5.2lf m/s x %5.2lf sec \n", x0, second_velocity, t);
+		printf("x = %5.2lf m \n", x);
 
-		printf("=================================================================\n");
-		printf("\n\n\n");
+        printf("\n");
 	}
 	if (answer == SHOW) {
-		printf("=========================   정 답   =============================\n");
+		printf("=========================   정 답   =============================\n\n");
 		printf("x를 계산합니다. \n");
-		printf("x = %5.2lf m \n", x);
+		printf("x = %5.2lf m \n\n", x);
 		printf("=================================================================\n");
 		printf("\n\n\n");
 	}
@@ -98,9 +97,7 @@ int main(void)
 
 
 	Excersize_2_24(1, 1);
-	//Excersize_2_2(1, 0);
-	//Excersize_2_12(1, 0);
-	//Excersize_2_22(1, 0);
+	
 
 
 
