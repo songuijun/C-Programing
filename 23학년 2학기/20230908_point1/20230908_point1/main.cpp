d3e18b81e4be9fb2a1e8e0343b0b81651320efd7
songuijun <125720533+songuijun@users.noreply.github.com>
2023-09-08 14:26:48 +0900

    Add files via upload

---
+++ 23학년 2학기/20230908_point1/20230908_point1/main.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+/*
+int main(void)
+{
+	int* numPtr;
+	int  num1 = 10;
+
+	numPtr = &num1;
+	printf("%d\n",numPtr);
+	printf("%d\n", &num1);
+
+	printf("%d\n", *numPtr);
+
+
+
+	return 0;
+};
+*/
+/*
+int main(void)
+{
+	int num = 123;
+	printf("%d\n", num);
+
+	return 0;
+};
+*/
+/*
+int main(void)
+{
+	int num = 123;
+	int* ptr = &num;
+	//int *ptr = &num;
+	//int * ptr = &num;
+
+	return 0;
+};
+*/
+/*
+int main(void){
+
+int arr1[] = {0, 1, 2};
+int *p1 = arr1;
+
+printf("%p\n", p1); //1
+printf("%p\n", (p1+1)); //2
+printf("%p\n", (p1+2)); //3
+
+return 0;
+};
+*/
+/*
+int main()
+{
+	int* numPtr;
+	int num1 = 10; 
+
+	numPtr = &num1;
+
+	printf("%p\n", numPtr);
+	printf("%p\n", &num1);
+	
+
+	return 0;
+}
+*/
