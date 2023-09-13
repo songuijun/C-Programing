4ba265c565ba0bd0100e9f7d15ff1b991a1c2609
songuijun <125720533+songuijun@users.noreply.github.com>
2023-09-13 14:29:07 +0900

    Add files via upload

---
+++ 23학년 2학기/20230911-2/swap/swap/FileName.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int* p, int* p1)
+{
+	int temp;
+	temp = *p;
+	*p = *p1;
+	*p1 = temp;
+
+}
+int main()
+{
+	int a = 5;
+	int b = 10;
+	printf("변경 전의 a의 값 : %d, b의 값 : %d\n", a, b);
+	swap(&a, &b);
+	printf("변경 후의 a의 값 : %d, b의 값 : %d\n", a, b);
+}
